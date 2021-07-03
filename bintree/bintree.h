784a974cc5fbe21ee75c32793f7297206cb3e09f
Cawa M <makartsev2015@gmail.com>
2021-07-03 23:06:41 +0300

    Сделал все методы, добавил тесты

--- bintree/bintree.h
+++ bintree/bintree.h
@@ -16,6 +16,7 @@ class MyBinTree
 			, left(nullptr)
 		{}
 	};
+	
 	Node<T>* root;
 public:
 	MyBinTree() 
@@ -23,32 +24,114 @@ public:
 	{}
 	~MyBinTree() 
 	{
-		deleteTree(&root);
+		deleteTree(root);
 	}
-	void add(T data) {
-		Node<T>* cur = root;
-		while (cur) {
-			Node<T>* node = cur;
-			if (data < node->inf) {
-				cur = node->left;
-			}
-			else if (data > node->inf) {
-				cur = node->right;
+
+	bool find(T key) 
+	{
+		return find(root, key);
+	}
+	void insert(T key) 
+	{
+		insert(root, key);
+	}
+	void remove(T key) 
+	{
+		remove(root, key);
+	}
+	int size() 
+	{
+		return size(root);
+	}
+
+private:
+	void deleteTree(Node<T>* root) 
+	{
+		if (root) 
+		{
+			deleteTree(root->left);
+			deleteTree(root->right);
+			delete root;
+		}
+		
+	}
+	bool find(Node<T>* stroot, T key) 
+	{
+		if (stroot == nullptr) 
+		{
+			return false;
+		}
+		if (stroot->inf == key) 
+		{
+			return true;
+		}
+		if (key < stroot->inf) 
+		{
+			return find(stroot->left, key);
+		}
+		return find(stroot->right, key);
+	}
+	Node<T>* insert(Node<T>* stroot, T key)
+	{
+		if (root == nullptr) 
+		{
+			root = new Node<T>(key);
+			return root;
+		}
+		if (find(stroot, key))
+		{
+			return stroot;
+		}
+		if (stroot == nullptr)
+		{			
+			return new Node<T>(key);
+		}
+		if (key < stroot->inf)
+		{
+			stroot->left = insert(stroot->left, key);
+		}
+		if (key > stroot->inf)
+		{
+			stroot->right = insert(stroot->right, key);
+		}
+		
+		return stroot;
+	}
+	Node<T>* remove(Node<T>* stroot, int key) {
+		if (stroot == nullptr || stroot->inf == key) return deleteRoot(stroot);
+		Node<T>* curr = stroot;
+		while (1) {
+			int x = curr->inf;
+			if (key < x) {
+				if (curr->left == nullptr || curr->left->inf == key) {
+					curr->left = deleteRoot(curr->left);
+					break;
+				}
+				curr = curr->left;
 			}
 			else {
-				return;
+				if (curr->right == nullptr || curr->right->inf == key) {
+					curr->right = deleteRoot(curr->right);
+					break;
+				}
+				curr = curr->right;
 			}
 		}
-		cur = new Node<T>(data);
+		return stroot;
 	}
-private:
-	void deleteTree(Node<T>** root) 
+	Node<T>* deleteRoot(Node<T>* stroot) {
+		if (!stroot || stroot->inf == 0)return nullptr;
+		if (stroot->right == nullptr) return stroot->left;
+		Node<T>* x = stroot->right;
+		while (x->left)x = x->left;
+		x->left = stroot->left;
+		return stroot->right;
+	}
+	int size(Node<T>* stroot)
 	{
-		if (*root) 
-		{
-			deleteTree(&((*root)->left));
-			deleteTree(&((*root)->right));
-			delete *root;
-		}
+		if (stroot == nullptr)
+			return 0;
+		else
+			return(size(stroot->left) + 1 + size(stroot->right));
 	}
 };
--- bintree/main.cpp
+++ bintree/main.cpp
@@ -1,11 +1,10 @@
 #include "bintree.h"
 
+
 using namespace std;
 
 int main() 
 {
-	MyBinTree<int> tree1;
-	
 
 	return 0;
 }
