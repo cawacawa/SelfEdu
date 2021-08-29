c1f823227edfb39a664311fd4076eea159883618
Cawa M <makartsev2015@gmail.com>
2021-08-29 20:04:57 +0300

    удаление готово

--- bintree/main.cpp
+++ bintree/main.cpp
@@ -12,20 +12,32 @@ ostream& operator<< (ostream& os, BST<T1>& bst)
 int main() 
 {
 	BST<int> bst;
-	
 	vector<int> toInsert;
-	
-	while (cin.get() != '\n') // Значения вводить через пробел (перед первым числом тоже пробел), считывает до энтера
+	while (cin.get() != '\n') 
 	{
 		int i = 0;
 		cin >> i;
 		toInsert.push_back(i);
 	}
-
     for (int i : toInsert)
 	{
 		bst.insert(i);
 	}
+
+	
+
+	vector<int> toDelete;
+	while (cin.get() != '\n')
+	{
+		int j = 0;
+		cin >> j;
+		toDelete.push_back(j);
+	}
+	for (int j : toDelete)
+	{
+		bst.remove(j);
+	}
+
 
 	cout << bst;
 
